f311f2089cf5abed534bb0f71b42a8b41812ec05
Artem Priadchenko <yorgan.info@gmail.com>
2023-07-04 20:07:34 +0300

    Завершение задания 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Controler
+    {
+
+        readonly int MemoriAccess;
+
+        public Controler(int MemoriAccess)
+        {
+            this.MemoriAccess = MemoriAccess;
+        }
+
+        public void MemoryLimitExceeded(string errorMessage)
+        {
+            if (MemoriAccess < GC.GetTotalMemory(false))
+            {
+                Console.WriteLine("{0}", errorMessage);
+                throw new Exception(errorMessage);
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Controler controler = new Controler(1000);
+
+            int[] ints = new int[10000];
+            
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = i;
+                controler.MemoryLimitExceeded("Перевовнено лимит вычерпано");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
